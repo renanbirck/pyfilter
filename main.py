9f8b922a997b6b896117cd9b8e997ee891d8d0e1
Renan Birck Pinheiro <renanbirck@chipinside.com.br>
2015-01-06 21:02:53 -0200

    test

--- main.py
+++ main.py
@@ -2,14 +2,28 @@
 
 VERSION = "0.0.0"
 
-from engine import calculation
+#from engine import analog, digital
 
+def do_analog_filter():
+    pass
+
+def do_digital_filter():
+    pass
 
 def main_menu():
     """ The main menu of pyfilter. Used for development before
         we have a GUI. """
     print("This is pyfilter {version}".format(version=VERSION))
+    print("Pick the type of filter: ")
+    filter_selector()
     return True
+
+def filter_selector():
+    choices = {'A': ('Analog', do_analog_filter()),
+               'D': ('Digital', do_digital_filter())}
+    for possible_choice in choices:
+        print("[{}] {}".format(possible_choice, choices[possible_choice][0]))
+    option = input("> ")
 
 if __name__ == '__main__':
     main_menu()
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
-import unittest
-from ..engine import calculation
+import unittest, sys, os
+
+# I don't really like this, but doesn't know any way
+sys.path.append('../engine')
+sys.path.append('..')
+
+from engine import calculation
 
 class TestCalculation(unittest.TestCase):
     """ Unit tests of the calculation engine.
@@ -15,6 +20,12 @@ class TestCalculation(unittest.TestCase):
 
     def test_get_computation_types(self):
         filters = self.Calculation.get_filter_types()
+        topologies = self.Calculation.get_topologies()
         self.assertEquals(filters, ["bandpass", "highpass",
                                     "lowpass", "notch",
                                     "allpass"])
+
+        self.assertEquals(filters)
+
+    def test_compute_filters(self):
+
