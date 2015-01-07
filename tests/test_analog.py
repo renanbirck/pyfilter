3c7916aa439adb0f0a01d54f170b195c8fe53126
Renan Birck Pinheiro <renan.ee.ufsm@gmail.com>
2015-01-07 14:29:30 -0200

    Change design.

---
+++ tests/test_analog.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import unittest
+
+class TestAnalog(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_filter_object(self):
+        pass
+
+    def test_get_topologies(self):
+        pass
+
+    def test_synth_filter(self):
+        pass
+
+
