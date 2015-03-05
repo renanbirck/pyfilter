ccc9b9b9662f72f1b740b0620bb0a802dab9f7a5
Renan Birck <renan.ee.ufsm@gmail.com>
2015-03-04 21:06:15 -0300

    GUI logics.

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -11,6 +11,10 @@ from PyQt4 import QtCore, QtGui
 from pyfilter_main_window import Ui_MainWindow
 
 class StartQT4(QtGui.QMainWindow):
+
+    config_dict = {}  # The dictionary of configuration
+                      # used by the validation routines.
+
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
@@ -36,6 +40,20 @@ class StartQT4(QtGui.QMainWindow):
                                    QtCore.SIGNAL("clicked()"),
                                    self.get_filter_kind)
 
+        for designParametersWidget in [self.ui.radioButton_NWn,
+                                       self.ui.radioButton_AttSpecs]:
+            QtCore.QObject.connect(designParametersWidget,
+                                   QtCore.SIGNAL("clicked()"),
+                                   self.configure_boxes_for_design_parameters)
+
+        QtCore.QObject.connect(self.ui.pushButton_Design,
+                               QtCore.SIGNAL("clicked()"),
+                               self.design_filter)
+
+        # Initial preparations
+        self.configure_boxes_for_design_parameters()
+        self.get_filter_kind()
+
         # Populate the color boxes
 
         color_names = ['Red', 'Green', 'Blue', 'Yellow', 'Black']
@@ -52,13 +70,52 @@ class StartQT4(QtGui.QMainWindow):
                                                     'PyFilter 0.1 (c) 2015 Renan Birck.')
         print("Hello World")
 
+    def configure_boxes_for_design_parameters(self):
+        """ This function configure the text boxes for the chosen design parameters.
+        """
+        if self.ui.radioButton_NWn.isChecked():
+            self.ui.label_opt1.setText("N: ")
+            self.ui.label_opt2.setText("Wn: ")
+            self.ui.label_opt3.hide()
+            self.ui.label_opt4.hide()
+            self.ui.plainTextEdit_opt1.setEnabled(True)
+            self.ui.plainTextEdit_opt2.setEnabled(True)
+            self.ui.plainTextEdit_opt3.hide()
+            self.ui.plainTextEdit_opt4.hide()
+
+            self.ui.plainTextEdit_opt1.setToolTip("The order. "
+                                                  "It must be an integer bigger than zero.")
+            self.ui.plainTextEdit_opt2.setToolTip("The natural frequency(ies)."
+                                                  "To the bandpass and bandstop cases, \n"
+                                                  "give 2 frequencies separated by space, \n"
+                                                  "like: 1 10, otherwise give one frequency.")
+
+            self.config_dict['mode'] = "N_WN"
+        elif self.ui.radioButton_AttSpecs.isChecked():
+            self.ui.label_opt1.setText("Fpass (Hz): ")
+            self.ui.label_opt2.setText("Fstop (Hz): ")
+            self.ui.label_opt3.setText("Rpass (dB): ")
+            self.ui.label_opt4.setText("Rstop (dB): ")
+            self.ui.label_opt3.show()
+            self.ui.label_opt4.show()
+            self.ui.plainTextEdit_opt3.show()
+            self.ui.plainTextEdit_opt4.show()
+
+            self.ui.plainTextEdit_opt1.setToolTip("The passband frequency, in hertz.")
+            self.ui.plainTextEdit_opt2.setToolTip("The stop frequency, in hertz.")
+            self.ui.plainTextEdit_opt3.setToolTip("The attenuation at passband, in dB.")
+            self.ui.plainTextEdit_opt4.setToolTip("The attenuation at stopband, in dB.")
+            self.config_dict['mode'] = "specs"
+
+        else:
+            raise ValueError("Somehow we chose something that can't be chosen!")
+
     def get_filter_kind(self):
         """ This function gets the appropriate filter type from
             the chosen options and configures the other radio buttons
             and controls accordingly. """
 
         choice = None
-
         if self.ui.radioButton_Bessel.isChecked():
             print("pick Bessel")
             choice = "bessel"
@@ -75,10 +132,13 @@ class StartQT4(QtGui.QMainWindow):
             print("pick elliptical")
             choice = "elliptical"
 
+        self.config_dict['filter_kind'] = choice
         self.setup_widgets(choice)
 
     def setup_widgets(self, choice):
         if choice == "butterworth":
+            # Butterworth filter needs to have the choice between match the
+            # passband or the stopband.
             self.ui.radioButton_matchPB.setEnabled(True)
             self.ui.radioButton_matchSB.setEnabled(True)
         else:
@@ -86,8 +146,30 @@ class StartQT4(QtGui.QMainWindow):
             self.ui.radioButton_matchSB.setEnabled(False)
 
         if choice == "cheby1":
+            # Chebyshev type-1 filter needs to have the option to
+            # configure the ripple.
             self.ui.label_pbRipple.setEnabled(True)
             self.ui.plainTextEdit_pbRipple.setEnabled(True)
+        else:
+            self.ui.label_pbRipple.setEnabled(False)
+            self.ui.plainTextEdit_pbRipple.setEnabled(False)
+
+    def design_filter(self):
+        """ This function designs the filter. """
+        # Parameter validation
+        if self.config_dict['mode'] == "N_WN":
+            # In N, Wn mode: N must be integer > 0
+            # Wn must be either 1 or 2 numbers > 0
+
+            try:
+                temp_N = int(self.ui.plainTextEdit_opt1.toPlainText())
+                if(temp_N <= 0):
+                    raise ValueError("Order must be bigger than 0!")
+            except:
+                QtGui.QMessageBox.critical(self,
+                                           'Parameter error',
+                                           'The filter order must be integer '
+                                           'and bigger than zero. Please fix.')
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
