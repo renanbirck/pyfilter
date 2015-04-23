68fe28cd2b38c026539edae289c8c4aac6d3493c
Renan Birck <renan.ee.ufsm@gmail.com>
2015-04-23 20:41:27 -0300

    Digital passing for Butterworth LP from N, Wn.

--- engine/digital.py
+++ engine/digital.py
@@ -3,33 +3,52 @@
 
 from math import pi
 from scipy import signal
-import custom
 from filter import Filter
+import custom
 
 hz_to_rad = lambda x: 2 * pi * float(x)
 
-class DigitalFilter(Filter):
-    """ Class of a general digital filter. """
+class IIRFilter(Filter):
 
-    sample_rate = 0
+    sample_rate = None
+    filter_type = None
 
-    def __init__(self, sample_rate):
-        if sample_rate <= 0:
-            raise ValueError("Sampling rate must be greater than zero!")
-        self.sample_rate = sample_rate
+    def compute_parameters(self):
+        self._compute_parameters()
 
+    def design(self):
+        self._design() # It computes Z, P, K for numerical stability.
+        self.B, self.A = signal.zpk2tf(self.Z, self.P, self.K)
 
-class ButterworthFilter(DigitalFilter):
+    def _compute_parameters(self):
+        raise ValueError("Please override me with your own _compute_parameters function!")
+
+    def _design(self):
+        raise ValueError("Please override me with your own _design function!")
+
+    def normalize_Wn(self):
+        if self.Wn > self.sample_rate:
+            raise ValueError("Frequency must be smaller than sample rate.")
+        self.Wn = self.Wn / (self.sample_rate/2)
+
+# MATLAB-ish filter design classes.
+class ButterworthFilter(IIRFilter):
+    target = None
+
+    def _design(self):
+        self.normalize_Wn()
+        self.Z, self.P, self.K = signal.butter(self.N, self.Wn,
+                                               self.filter_kind, analog=False,
+                                               output='zpk')
+
+class ChebyshevIFilter(IIRFilter):
     pass
 
-class ChebyshevIFilter(DigitalFilter):
+class ChebyshevIIFilter(IIRFilter):
     pass
 
-class ChebyshevIIFilter(DigitalFilter):
+class EllipticalFilter(IIRFilter):
     pass
 
-class EllipticalFilter(DigitalFilter):
-    pass
-
-class BesselFilter(DigitalFilter):
+class BesselFilter(IIRFilter):
     pass
--- tests/test_digital.py
+++ tests/test_digital.py
@@ -17,24 +17,40 @@ class TestDigital(unittest.TestCase):
     filter_under_test = None
     sample_rate = 20000
 
-    def setUp(self):
-        """ Instance the filter. """
-        self.filter_under_test = digital.DigitalFilter(sample_rate=self.sample_rate)
-        self.assertEqual(self.filter_under_test.sample_rate, self.sample_rate)
+    def test_butterworth_N_Wn(self):
+        butterworth = digital.ButterworthFilter()
+        butterworth.sample_rate = self.sample_rate
 
-    def test_construct_filters(self):
-        """ Try constructing some filters. """
-        butterworth = digital.ButterworthFilter(sample_rate=self.sample_rate)
-        cheby1 = digital.ChebyshevIFilter(sample_rate=self.sample_rate)
-        cheby2 = digital.ChebyshevIIFilter(sample_rate=self.sample_rate)
-        elliptical = digital.EllipticalFilter(sample_rate=self.sample_rate)
-        bessel = digital.BesselFilter(sample_rate=self.sample_rate)
+        butterworth.N = 2
+        butterworth.Wn = 100
+        butterworth.filter_kind = "lowpass"
+        butterworth.design()
 
-        self.assertIsInstance(butterworth, digital.ButterworthFilter)
-        self.assertIsInstance(cheby1, digital.ChebyshevIFilter)
-        self.assertIsInstance(cheby2, digital.ChebyshevIIFilter)
-        self.assertIsInstance(elliptical, digital.EllipticalFilter)
-        self.assertIsInstance(bessel, digital.BesselFilter)
+        target_B_coefs = [241.359049041961e-006, 482.718098083923e-006, 241.359049041961e-006]
+        target_A_coefs = [1.00000000000000e+000, -1.95557824031504e+000, 956.543676511203e-003]
+
+        for idx, coef in enumerate(target_B_coefs):
+            self.assertAlmostEqual(butterworth.B[idx],
+                                   coef, places=4)
+
+        for idx,coef in enumerate(target_A_coefs):
+            self.assertAlmostEqual(butterworth.A[idx],
+                                   coef, places=4)
+
+        pass
+
+    def test_bessel_N_Wn(self):
+        pass
+
+    def test_cheby1_N_Wn(self):
+        pass
+
+    def test_cheby2_N_Wn(self):
+        pass
+
+    def test_ellip_N_Wn(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
